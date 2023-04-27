090fe78737a305d0c7fc4bc84fbe0ebde1ee99f5
Alina <125072995+alinefaiz@users.noreply.github.com>
2023-04-27 03:05:40 +0300

    Usability update

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 # pip install pyTelegramBotAPI
 # pip install Faker
+from telebot import TeleBot, types
 from secrets import token_urlsafe
 import json
 from faker import Faker
 
-bot = TeleBot(token='ТВОЙ_ТОКЕН', parse_mode='html') # создание бота
+bot = TeleBot(token='', parse_mode='html') # создание бота
 faker = Faker('ru_RU')
 
 @bot.message_handler(commands=['start'])# обработчик команды '/start'
@@ -14,62 +15,52 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('Словарик')
     item2 = types.KeyboardButton('Сгенерировать тестовые данные пользователя')
-    item3 = types.KeyboardButton('В главное меню')
-    markup.add(item1, item2, item3)
-    bot.send_message(message.chat.id, 'Привет! Ты можешь спросить у меня определение какого-нибудь слова. \nИли же я могу сгенерировать тестовые данные пользователя. \nВыбери то, что тебе нужно!', reply_markup=markup)
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, 'Привет! Ты можешь спросить у меня определение какого-нибудь слова. \nИли же я могу сгенерировать тестовые данные пользователя для регистрации. \nВыбери то, что тебе нужно!', reply_markup=markup)
 
-DEFINITOINS = {#Словарик
+DEFINITOINS = {
     'регресс': 'Проверить что новый функционал не сломал существующий',
-    'новое_слово': 'Определение',
+    'слово':'Определение слова',
 }
 
 # обработчик всех остальных сообщений
 @bot.message_handler()
 
 def message_handler(message: types.Message):
-    if message.text == 'В главное меню':
+    if message.text == 'Словарик':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton('Словарик')
-        item2 = types.KeyboardButton('Сгенерировать тестовые данные пользователя')
-        item3 = types.KeyboardButton('В главное меню')
-        markup.add(item1, item2, item3)
-        bot.send_message(message.chat.id, 'Ты в главном меню. \nВыбери то, что тебе нужно!', reply_markup=markup)
-    elif message.text == 'Словарик':
-        bot.send_message(message.chat.id, 'Введи слово.')
+        back = types.KeyboardButton('В главное меню')
+        markup.add(back)
+        bot.send_message(message.chat.id, 'Введи слово. \nИли нажми "В главное меню", чтобы вернуться.', reply_markup=markup)
         bot.register_next_step_handler(message, word)
     elif message.text == 'Сгенерировать тестовые данные пользователя':
-        bot.send_message(message.chat.id, 'Введи количество пользователей. (Максимум 20)')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        back = types.KeyboardButton('В главное меню')
+        markup.add(back)
+        bot.send_message(message.chat.id, 'Введи количество пользователей. (Максимум 20) \nИли нажми "В главное меню", чтобы вернуться.', reply_markup=markup)
         bot.register_next_step_handler(message, generate_user_data)
+    elif message.text == 'В главное меню':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Словарик')
+        btn2 = types.KeyboardButton('Сгенерировать тестовые данные пользователя')
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, 'Ты в главном меню. \nВыбери то, что тебе нужно!', reply_markup=markup)
     else: # если текст не совпал ни с одной из кнопок выводим ошибку
-        bot.send_message(
-        chat_id=message.chat.id,
-        text='Не понимаю тебя :(',
-    )
+        bot.send_message(chat_id=message.chat.id,text='Не понимаю тебя :(',)
         
 def word(message):
     if message.text == 'В главное меню':
         bot.register_next_step_handler(message, message_handler(message))
         return
-    elif message.text == 'Сгенерировать тестовые данные пользователя':
-        bot.register_next_step_handler(message, message_handler(message))
-        return
-    definition = DEFINITOINS.get(
-    message.text.lower(), # приводим текст сообщения к нижнему регистру
-    )
+    definition = DEFINITOINS.get(message.text.lower(),) # приводим текст сообщения к нижнему регистру
     if definition is None:
-        bot.send_message(
-            chat_id=message.chat.id,
-            text='Я пока не знаю такого определения.',
-        )
+        bot.send_message(chat_id=message.chat.id,text='Я пока не знаю такого определения.',)
         message.text='Словарик'
         bot.register_next_step_handler(message, message_handler(message))
         return
     # если ключевая фраза была найдена, формируем текст сообщения и отправляем его
     # если перед строкой поставить букву f, то в фигурных скобках {} можно использовать переменные :)
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=f'Определение:\n<code>{definition}</code>',
-    )
+    bot.send_message(chat_id=message.chat.id,text=f'Определение:\n<code>{definition}</code>',)
     message.text='Словарик'
     bot.register_next_step_handler(message, message_handler(message))
 
@@ -78,10 +69,7 @@ def generate_user_data(message):
     if message.text == 'В главное меню':
         bot.register_next_step_handler(message, message_handler(message))
         return
-    elif message.text == 'Словарик':
-        bot.register_next_step_handler(message, message_handler(message))
-        return
-    elif message.text.isdigit() and int(message.text)<=20 and int(message.text)>0:
+    if message.text.isdigit() and int(message.text)<=20 and int(message.text)>0:
         payload_len = int(message.text)
     else:
         bot.send_message(chat_id=message.chat.id, text='Я так не могу :(')
